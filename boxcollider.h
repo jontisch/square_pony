724e01baf73c94a6decef64f6cd95c2b4fc203bd
jontisch <john.bergelin@gmail.com>
2019-05-23 19:42:23 +0200

    Collider class modified

--- boxcollider.h
+++ boxcollider.h
@@ -1,11 +1,25 @@
 #ifndef BOXCOLLIDER_H
 #define BOXCOLLIDER_H
+#include <QPointF>
+#include <stdint.h>
 
+#define N_COLLISIONPOINTS 8
 
 class Boxcollider
 {
 public:
     Boxcollider();
+
+private:
+    QPointF _collisionPoints[N_STATES][N_COLLISIONPOINTS];
+    QPointF _collisionOffset[N_STATES][2];
+
+
+    uint16_t _nStates;
+
+    bool _xBlocked;
+    bool _yTopBlocked;
+    bool _yBottomBlocked;
 };
 
 #endif // BOXCOLLIDER_H
